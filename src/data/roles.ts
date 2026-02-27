450086ec489cb98c12e7a77f781b7adeb384993d
Merch04 <kirill004mel@mail.ru>
2026-02-28 01:09:14 +0700

    Stop tracking src/data directory

--- src/data/roles.ts
+++
@@ -1,8 +0,0 @@
-export const roles: Record<string, string> = {
-	// academy: '1476258511200583680', //merch server roles
-	// main: '1476258329255870768',//merch server roles
-	academy: '1385076942671188120',
-	main: '1383397239258480701',
-	gold: '1426009097115009104',
-	old: '1384937082941870183',
-} as const;
